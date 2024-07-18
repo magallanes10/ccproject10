fe869129dc85be2c9b6b2dcd197df24cd50dece0
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-07-18 14:13:52 +0800

    Update removebg.js

--- routes/removebg.js
+++ routes/removebg.js
@@ -1,23 +1,65 @@
 const express = require('express');
-const { removebg } = require('betabotz-tools');
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
 
-const removebgRouter = express.Router();
+const router = express.Router();
 
-removebgRouter.get('/rbg', async (req, res) => {
-  const { imageUrl } = req.query;
+const apiKeys = [
+  "DABuioDTVc19SqDGaWS7CjYy", "ToQX2FRYSXjWGSvmL5vNCzvT", "K2mEzPfotSS9wa5HdqadHhr6", "mQuPKWdMNTFjw1g3LhmJBdH4", "zCUdE9DNjNGiUat2HGvpvSut", "AvrQA8iowvh4fSZhA7QksL7N", "5Wc8hyf8hMLk8kba3ZbEfiXb"
+];
 
-  if (!imageUrl) {
-    return res.status(400).json({ error: 'imageUrl parameter is required' });
+function getRandomApiKey() {
+  const randomIndex = Math.floor(Math.random() * apiKeys.length);
+  return apiKeys[randomIndex];
+}
+
+async function removeBg(imageURL) {
+  const formData = new FormData();
+  formData.append("size", "auto");
+  formData.append("image_url", imageURL);
+
+  const apiKey = getRandomApiKey();
+
+  try {
+    const response = await axios.post("https://api.remove.bg/v1.0/removebg", formData, {
+      headers: {
+        ...formData.getHeaders(),
+        "X-Api-Key": apiKey,
+      },
+      responseType: 'arraybuffer',
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`${error.response.status}: ${error.response.statusText}`);
+  }
+}
+
+router.get('/removebg', async (req, res) => {
+  const imageURL = req.query.url;
+
+  if (!imageURL) {
+    return res.status(400).send('Image URL is required');
   }
 
   try {
-    const results = await removebg(imageUrl);
-    console.log(results);
-    res.json(results); 
+    const rbgResultData = await removeBg(imageURL);
+    const outputFilePath = path.join(__dirname, 'no-bg.png');
+    fs.writeFileSync(outputFilePath, Buffer.from(rbgResultData));
+    res.sendFile(outputFilePath, (err) => {
+      if (err) {
+        console.error('Error sending file:', err);
+        res.status(500).send('Error sending file');
+      } else {
+        console.log('Image sent successfully!');
+      }
+    });
   } catch (error) {
     console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).send(`Error: ${error.message}`);
   }
 });
 
-module.exports = removebgRouter;
+module.exports = router;
